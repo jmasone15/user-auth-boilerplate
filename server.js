f62a69ad05c303239d55631bd328a82f5af7d400
Jordan Masone <jordanmasone1@gmail.com>
2021-03-19 16:39:28 -0400

    Mongoose setup.

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
+// Database Connection
+// Make sure to put the db name here.
+mongoose.connect("mongodb://localhost/mernUserAuth", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+}, (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB");
+});
+
+// Starts the server.
+app.listen(PORT, () => console.log(`Running on port: ${PORT}🌎`));
