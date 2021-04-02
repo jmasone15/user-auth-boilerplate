2f6c7727e1b4dd4b1ec14179324499864c9da007
Jordan Masone <jordanmasone1@gmail.com>
2021-04-02 14:01:35 -0400

    Heroku

--- client/src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-const proxy = require("http-proxy-middleware");
-
-module.exports = function (app) {
-    app.use(
-        proxy(["/auth", "/thing"], { target: "https://desolate-citadel-94962.herokuapp.com/" })
-    );
-};
